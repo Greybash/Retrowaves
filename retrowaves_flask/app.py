6219d631e988920b37f48178c0a7b50125dc827f
Prem <premdas200326@gmail.com>
2025-03-15 21:29:03 +0530

    Update app.py

--- retrowaves_flask/app.py
+++ retrowaves_flask/app.py
@@ -79,7 +79,12 @@ def profile():
     user_data = requests.get("https://api.spotify.com/v1/me", headers=headers).json()
 
     username = user_data.get('display_name', 'Unknown User')
-    profile_image = user_data.get('images', [{}])[0].get('url', url_for('static', filename='profile.png'))
+    profile_image = user_data.get('images', [])
+    if profile_image:
+        profile_image = profile_image[0].get('url', url_for('static', filename='profile.png'))
+    else:
+        profile_image = url_for('static', filename='profile.png')  # Use default image
+
 
     return render_template('profile.html', username=username, profile_image=profile_image)
 
